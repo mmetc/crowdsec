e43ead33b53fddcfb3ac8a9dd9b9114192202006
Marco Mariani <marco@crowdsec.net>
2022-10-25 04:45:32 +0200

    laskdfjals

---
+++ .github/commitlint.config.js
@@ -0,0 +1,9 @@
+// blallblbalablablbal
+module.exports = {
+    extends: [
+        "@commitlint/config-conventional"
+    ],
+    rules: {
+        "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "test"]],
+    }
+}
