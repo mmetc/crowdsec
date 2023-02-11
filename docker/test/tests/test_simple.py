69726404ac09475e481c05a9b4b6ab2127ddb68f
Marco Mariani <marco@crowdsec.net>
2023-02-11 23:03:10 +0100

    example with log_waiters() iterator + context manager

--- docker/test/tests/test_simple.py
+++ docker/test/tests/test_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from pytest_cs import log_lines, wait_for_log, Status
+from pytest_cs import log_waiters, Status
 
 import pytest
 
@@ -8,14 +8,17 @@ pytestmark = pytest.mark.docker
 
 
 def test_container(container):
-    # XXX: not crowdsec
     with container(image='hello-world', wait_status=Status.EXITED) as cont:
-        assert 'Hello from Docker!' in log_lines(cont)
+        for waiter in log_waiters(cont, timeout=5):
+            with waiter as matcher:
+                matcher.fnmatch_lines(["Hello from Docker!"])
 
 
 def test_crowdsec(crowdsec):
     with crowdsec() as cont:
-        wait_for_log(cont, "Starting processing data")
+        for waiter in log_waiters(cont, timeout=5):
+            with waiter as matcher:
+                matcher.fnmatch_lines(["*Starting processing data*"])
         res = cont.exec_run('sh -c "echo $CI_TESTING"')
         assert res.exit_code == 0
         assert 'true' == res.output.decode().strip()
