6779cadcf1f266b6c9baa6ea4623ced1bf25775b
Marco Mariani <marco@crowdsec.net>
2023-12-19 10:02:48 +0100

    wip

--- test/localstack/scripts/init_script.sh
+++ test/localstack/scripts/init_script.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#apt install netcat-openbsd -y
-#
-## Wait for LocalStack to be ready
-#while ! nc -z localstack 4566; do
-#  sleep 1
-#done
-
 # Create Kinesis streams
 aws --endpoint-url=http://localstack:4566 --region us-east-1 kinesis create-stream --stream-name stream-1-shard --shard-count 1
 aws --endpoint-url=http://localstack:4566 --region us-east-1 kinesis create-stream --stream-name stream-2-shards --shard-count 2
